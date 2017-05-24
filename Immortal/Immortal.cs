bbd45d678d9551d77bcf9b3027185f4adf171904
Heorhin, Dmytro <Dmytro.Heorhin@wolterskluwer.com>
2017-05-24 18:33:42 +0300

    init

---
+++ Immortal/Immortal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Immortal
+{
+    public class Immortal
+    {
+        private int _finalizationCount;
+        ~Immortal()
+        {
+            Console.WriteLine("Immortal object is having it's finalizer called {0}th time", ++_finalizationCount);
+            GC.ReRegisterForFinalize(this);
+        }
+    }
+}
---
+++ Immortal/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Immortal
+{
+    internal class Program
+    {
+        private static Immortal _immortalObject;
+
+        static void Main(string[] args)
+        {
+            Allocate();
+            Nullify();
+            for (var i = 1; i < 10000; i++)
+            {
+                var largeObject = new Large(i);
+            }
+        }
+
+        private static void Allocate()
+        {
+            _immortalObject = new Immortal();
+        }
+
+        private static void Nullify()
+        {
+            _immortalObject = null;
+        }
+    }
+}
